7c455d63286fef66b566ae255ce3174ca8d4d52d
ssenturk <ssenturk@tp-otc.com>
2023-08-14 18:48:48 +0300

    slider function changed

---
+++ shared/enums/images.ts
@@ -0,0 +1,5 @@
+export const Images: string[] = [
+  'assets/dist/images/banner-01.jpg',
+  'assets/dist/images/banner-02.jpg',
+  'assets/dist/images/banner-03.jpg',
+];
--- src/app/components/home/slider/slider.component.ts
+++ src/app/components/home/slider/slider.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Images } from 'shared/enums/images';
 
 @Component({
   selector: 'app-slider',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.css']
+  styleUrls: ['./slider.component.css'],
 })
 export class SliderComponent {
+  currentImageIndex: number = 0;
+  images: string[] = Images;
+  prevImage(): void {
+    this.currentImageIndex =
+      (this.currentImageIndex - 1 + this.images.length) % this.images.length;
+  }
 
+  nextImage(): void {
+    this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+  }
 }
